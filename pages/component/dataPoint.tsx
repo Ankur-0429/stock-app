d1df6aeced31a7faca22d933ac9ca600ff1aa88c
Ankur Ahir <ankurahir0429@gmail.com>
2021-10-06 12:20:15 -0700

    green/red colors

--- pages/component/dataPoint.tsx
+++ pages/component/dataPoint.tsx
@@ -2,50 +2,70 @@ import React from 'react';
 import { Line } from "react-chartjs-2"
 
 
+const grabArr = (data) => {
+  let labelArr = []
+  let labelD = []
+  for (let i = 0; i < data.length; i++) {
+    labelArr.push(data[i].date.substring(0,10))
+    labelD.push(data[i].open)
+  }
+  return [labelArr.reverse(), labelD.reverse()]
+}
 
-const DataPoint = ({data}:any) => {
-    
-    let labelArr = []
-    let labelD = []
-    for (let i = 0; i < data.length; i++) {
-        labelArr.push(data[i].date)
-        labelD.push(data[i].open)
-    }
 
-    const graph = {
-        
-        labels: labelArr,
-        
-        datasets: [
-          {
-            label: data[0].symbol,
-            fill: false,
-            lineTension: 0,
-            backgroundColor: 'rgba(75,192,192,0.4)',
-            borderColor: 'rgba(75,192,192,1)',
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: 'rgba(75,192,192,1)',
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: labelD
-          }
-        ]
+const DataPoint = ({ data }: any) => {
+
+
+  let temp = grabArr(data)
+  const labelArr = temp[0]
+  const labelD = temp[1]
+  let color = 'green'
+  
+  if (labelD[0] > labelD[labelD.length-1]) {
+    color = 'red'
+  }
+
+  let LabelSymbol = ''
+  if (data.length == 0){
+    LabelSymbol = 'N/A'
+  }
+  else {
+    LabelSymbol = data[0].symbol
+  }
+  const graph = {
+
+    labels: labelArr,
+
+    datasets: [
+      {
+        label: LabelSymbol,
+        fill: false,
+        lineTension: 0,
+        backgroundColor: color,
+        borderColor: color,
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: color,
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: color,
+        pointHoverBorderColor: color,
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 1000,
+        data: labelD
       }
-    
-    return (
-        <Line
-            data={graph}
-        />
-    )
+    ]
+  }
+
+  return (
+    <Line
+      data={graph}
+    />
+  )
 }
 
 export default DataPoint
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,31 @@
-import React, { useEffect, useState } from 'react'
+import React, { CSSProperties, useState } from 'react'
 import DataPoint from './component/dataPoint'
 
-const url = 'https://stock-app-backend.ankurahir.repl.co/api/stocks/query?symbol=aapl'
+const url = 'http://localhost:5000/api/stocks/query/?symbol='
+const period = 'd'
+
+const inputStyle:CSSProperties = {
+  padding: '10px',
+  marginLeft: '10px',
+  marginTop: '10px',
+  border: '0',
+  boxShadow : '0 0 15px 4px rgba(0,0,0,0.06)'
+}
 
 function App() {
 
-  useEffect(() => {
-    fetch(url)
+  const fetcher = (symbol) => {
+    fetch(url+symbol+"&period="+period)
       .then(res => res.json())
       .then(data => setData(data))
-  }, [])
-
-
+  }
   const [data, setData] = useState([])
-
+    
   return (
     <main className='App'>
+      <form>
+        <input onChange={(e)=> fetcher(e.target.value)} style={inputStyle} />
+      </form>
       <div>
         <DataPoint data={data} />
       </div>
