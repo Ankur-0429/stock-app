a4fa7e993790b5910c0023d58faba3ecc097b7d8
Ankur Ahir <ankurahir0429@gmail.com>
2021-10-08 11:41:31 -0700

    added timeline functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,30 +2,50 @@ import React, { CSSProperties, useState } from 'react'
 import DataPoint from './component/dataPoint'
 
 const url = 'http://localhost:5000/api/stocks/query/?symbol='
-const period = 'd'
 
 const inputStyle:CSSProperties = {
   padding: '10px',
   marginLeft: '10px',
   marginTop: '10px',
-  border: '0',
+  border: '1px solid black',
   boxShadow : '0 0 15px 4px rgba(0,0,0,0.06)'
 }
 
+let arr = []
 function App() {
 
-  const fetcher = (symbol) => {
+  const fetcher = (symbol, period) => {
     fetch(url+symbol+"&period="+period)
       .then(res => res.json())
-      .then(data => setData(data))
+      .then(data => {setData(data); arr = [...data]})
   }
+
+  const timeSet = (yearDiff:number) => {
+    setData(arr.filter((e)=> currentYear - parseInt(e.date.substring(0,4)) <= yearDiff))
+  }
+
+  const [symbol, setSymbol] = useState('aapl')
+  const [period, setPeriod] = useState('m')
   const [data, setData] = useState([])
+
+  var currentYear:number = 2021
+  
+  // if (data !== []) {
+  //   currentYear = parseInt(data[data.length-1].date.substring(0,10))
+  // }
+
+  const years = [5,3,1]
+ 
     
   return (
     <main className='App'>
       <form>
-        <input onChange={(e)=> fetcher(e.target.value)} style={inputStyle} />
+        <input onChange={(e)=> {setSymbol(e.target.value); fetcher(e.target.value, period)}} style={inputStyle} />
       </form>
+      
+      <button onClick={()=>{timeSet(5)}}>5 year</button>
+      <button onClick={()=>{timeSet(3)}}>3 year</button>
+      <button onClick={()=>{timeSet(1)}}>1 year</button>
       <div>
         <DataPoint data={data} />
       </div>
