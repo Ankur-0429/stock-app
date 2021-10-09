cd0a476f6a113b61847fe53e5e08825c47985ab8
Albert Lee <allilee@ucsc.edu>
2021-10-09 12:45:13 -0700

    Added documentation for globalStyles.tsx

--- component/globalStyles.tsx
+++ component/globalStyles.tsx
@@ -1,3 +1,12 @@
+/**
+ *  # ToggleSwitch.tsx
+ *  This program defines the theme toggle switch. So the user can change between light and dark mode
+ *  
+ *  ## Authors
+ *  - Ankur Ahir
+ *  
+ */
+
 import { createGlobalStyle} from "styled-components"
 
 export const GlobalStyles = createGlobalStyle`
