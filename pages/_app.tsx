2786ed8cc40b22c9b7566d1c987cecafe2c9f047
Albert Lee <allilee@ucsc.edu>
2021-10-09 12:45:59 -0700

    Added documentation for _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,13 @@
+/**
+ *  # _app.tsx
+ *  This is the main file that operates our webpage
+ *  
+ *  ## Authors
+ *  - Ankur Ahir
+ *  - Albert Lee
+ * 
+ */
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
