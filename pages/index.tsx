7161a37dbc1cb5b43d26ec1c8294058f789b7f37
Albert Lee <allilee@ucsc.edu>
2021-10-10 13:39:27 -0700

    Updated packages

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ import SearchStyles from '../styles/SearchBar.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
-const url = 'http://localhost:8080/api/stocks/'
+const url = 'https://stock-app-alpha.vercel.app/'
 
 const inputStyle: CSSProperties = {
   marginTop: '10px',
